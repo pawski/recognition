360258e6b9c0f2fbde36b38354cf8802b557f854
Pawel Czyzewski <pawel.czyzewski@enginewerk.com>
2017-08-25 13:40:38 +0200

    Separate transform execution among goroutines

--- cmd/main.go
+++ cmd/main.go
@@ -1,19 +1,18 @@
 package main
 
 import (
-	"fmt"
 	"image"
-	"image/color"
+	//"image/color"
 	"log"
 	"github.com/disintegration/imaging"
 	"os"
 )
 
 func main() {
-	fmt.Printf("Strating app.\n")
+	log.Print("Strating app.\n")
 
 	argsWithoutProg := os.Args[1:]
-	fmt.Println(argsWithoutProg)
+	log.Print(argsWithoutProg)
 
 	imageSourcePath := os.Args[1];
 
@@ -23,20 +22,65 @@ func main() {
 		log.Fatalf("Open failed: %v", err)
 	}
 
-	// Create a blurred version of the image.
-	img1 := imaging.Blur(src, 2)
+	done := make(chan bool, 1)
 
-	// Create a grayscale version of the image with higher contrast and sharpness.
-	img2 := imaging.Grayscale(src)
-	img2 = imaging.AdjustContrast(img2, 20)
-	img2 = imaging.Sharpen(img2, 2)
+	go func(src image.Image){
+		transform(src, "blur")
+		done <- true
+	}(src)
 
-	// Create an inverted version of the image.
-	img3 := imaging.Invert(src)
+	go func(src image.Image, done chan bool) {
+		transform(src, "gray_scale")
+		done <- true
+	}(src, done)
 
-	// Create an embossed version of the image using a convolution filter.
-	img4 := imaging.Convolve3x3(
-		src,
+	go func(src image.Image) {
+		transform(src, "contrast")
+		done <- true
+	}(src)
+
+	go func(src image.Image) {
+		transform(src, "sharpen")
+		done <- true
+	}(src)
+
+	go func(src image.Image) {
+		transform(src, "invert")
+		done <- true
+	}(src)
+
+	go func(src image.Image) {
+		transform(src, "emboss")
+		done <- true
+	}(src)
+
+	for i := 0; i < 6; i++ {
+		select {
+		case <-done:
+		}
+	}
+}
+
+func transform(img image.Image, processType string) {
+
+	log.Printf("%s\n", processType + " start")
+
+	var processedImage image.Image
+
+	switch processType {
+	case "gray_scale":
+		processedImage = imaging.Grayscale(img)
+	case "blur":
+		processedImage = imaging.Blur(img, 2)
+	case "contrast":
+		processedImage = imaging.AdjustContrast(img, 20)
+	case "sharpen":
+		processedImage = imaging.Sharpen(img, 2)
+	case "invert":
+		processedImage = imaging.Invert(img)
+	case "emboss":
+		processedImage = imaging.Convolve3x3(
+		img,
 		[9]float64{
 			-1, -1, 0,
 			-1, 1, 1,
@@ -44,23 +88,15 @@ func main() {
 		},
 		nil,
 	)
+	default:
+		log.Panic("Unknown transform type")
+	}
 
-	// Create a new image and paste the four produced images into it.
-	dst := imaging.New(512, 512, color.NRGBA{0, 0, 0, 0})
-	dst = imaging.Paste(dst, img1, image.Pt(0, 0))
-	dst = imaging.Paste(dst, img2, image.Pt(0, 256))
-	dst = imaging.Paste(dst, img3, image.Pt(256, 0))
-	dst = imaging.Paste(dst, img4, image.Pt(256, 256))
-
-	// Save the resulting image using JPEG format.
-	err = imaging.Save(dst, "./cache/collage.jpg")
-
-	imaging.Save(img1, "./cache/blur.jpg")
-	imaging.Save(img2, "./cache/gray_scale.jpg")
-	imaging.Save(img3, "./cache/contrast.jpg")
-	imaging.Save(img4, "./cache/sharpen.jpg")
+	err := imaging.Save(processedImage, "./cache/" + processType + ".jpg")
 
 	if err != nil {
 		log.Fatalf("Save failed: %v", err)
 	}
+
+	log.Printf("%s\n", processType)
 }
